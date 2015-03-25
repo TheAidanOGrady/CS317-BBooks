4151a351c1892b9b0dec0b14c79aa49003630222
Aidan O'Grady <theaidanogrady@gmail.com>
2015-03-25 11:40:56 +0000

    Set up materialize and basic home page

---
+++ src/js/init.js
@@ -0,0 +1,11 @@
+/*jslint node: true, browser: true */
+/*global $, jQuery*/
+"use strict";
+
+(function ($) {
+    $(function () {
+
+        $('.button-collapse').sideNav();
+
+    }); // end of document ready
+}(jQuery)); // end of jQuery name space
