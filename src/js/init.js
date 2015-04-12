7ae0e4b39db26544e46748f1759eba13626ad6fa
Aidan O'Grady <theaidanogrady@gmail.com>
2015-04-12 20:01:46 +0100

    Minor style changes.

--- src/js/init.js
+++ src/js/init.js
@@ -7,9 +7,9 @@
 
         $('.button-collapse').sideNav();
 
-		$(document).ready(function(){
-			$('.slider').slider({full_width: true});
-		});
+        $(document).ready(function () {
+            $('.slider').slider({full_width: true});
+        });
     }); // end of document ready
 }(jQuery)); // end of jQuery name space
 
